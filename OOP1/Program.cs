5421a509beb03a04fddcd2e59c1c31d9aa774f0e
tolga hastaoğlu <tolga_hastaoglu@hotmail.com>
2021-01-20 22:30:35 +0300

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice =500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product {Id=2,CategoryId=3,ProductName="Ayakkabı",UnitPrice=250,UnitsInStock=25 };
+            // Bu iki yazım arasında hiç bir fark yoktur..
+
+            // case sensitive Büyük küçük harf duyarlı
+            //pascal case  // camel case
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+            
+    }
+}
